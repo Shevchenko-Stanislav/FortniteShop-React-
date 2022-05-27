27a0cfc34db60eae2955b30233d040c472b1331f
Stanislav <sa_shev@mail.ru>
2022-05-27 22:48:05 +0300

    update Header and Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-function Footer () {
-    return(
+function Footer() {
+    return (
         <footer className="page-footer blue darken-3">
-          <div className="footer-copyright">
-            <div className="container">
-                © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">
-                    More Links
-                </a>
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a className="grey-text text-lighten-4 right" 
+                    href="https://github.com/Shevchenko-Stanislav/fortnite-shop-react"
+                    target="_blank" rel="noreferrer">
+                        Repository
+                    </a>
+                </div>
             </div>
-          </div>
         </footer>
     )
 }
 
-export {Footer}
+export { Footer }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,13 @@ function Header() {
     return(
         <nav className="blue darken-3">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">FORTNITE SHOP</a>
+                <a href="." className="brand-logo">FORTNITE SHOP</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#!" className="aaa">Some Link</a></li>
+                    <li>
+                        <a href="https://github.com/Shevchenko-Stanislav/fortnite-shop-react" target="_blank" rel="noreferrer">
+                            Repository
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
